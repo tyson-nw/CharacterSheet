cf123bc48489bd2f7f4559ee179748f0a22d4c94
Tyson VanOverhill <tyson.vanover@gmail.com>
2024-08-13 22:46:09 +0000

    Parsing features to $out done

--- app/Http/Controllers/CharacterSheetController.php
+++ app/Http/Controllers/CharacterSheetController.php
@@ -8,6 +8,8 @@ use App\Models\Armor;
 use App\Models\Perks;
 use App\Models\Weapons;
 
+use function PHPUnit\Framework\stringContains;
+
 class CharacterSheetController extends Controller
 {
     /**
@@ -28,6 +30,12 @@ class CharacterSheetController extends Controller
             "Treasure" => $character["Treasure"]
         );
 
+        if(isset($out["Expertises"]["Bows"])){
+            unset($out["Expertises"]["Bows"]);
+            $out["Expertises"]["Longbow"] = true;
+            $out["Expertises"]["Shortbow"] = true;
+        }
+
         $out["Tier"] = 1;
 
         $out["Defenses"] = array(
@@ -45,9 +53,10 @@ class CharacterSheetController extends Controller
         $armor_model = new Armor();
         $perks_model = new Perks();
 
+        $out["Spellcasting Feature"] = null;
         $out["Perks"] = array();
 
-        $out["Manuvers"] = ["Stride"=>"Move 6 spaces."];
+        $out["Maneuvers"] = ["Stride"=>"Move 6 spaces."];
         $out["Responses"] = ["Movement Response" => "Make a melee attack when a target leaves a threatened square."];
 
         $weapon_actions = [];
@@ -58,17 +67,27 @@ class CharacterSheetController extends Controller
             if (isset($weapons_model->data[$equip])){
                 $action = array();
                 $action["Title"] = $equip;
+
+                $skill_bonus = 0;
+                if(isset($out["Skills"]["Weapons"])){
+                    $skill_bonus = $out["Skills"]["Weapons"];
+                }
+                $expertise_bonus = 0;
+                if(isset($out["Expertises"][$equip])){
+                    $expertise_bonus = 2;
+                }
+
                 if (in_array("Finesse", $weapons_model->data[$equip]["Tags"])){
-                    $action["Bonus"] = $out["Stats"]["Dexterity"] + $out["Skills"]["Weapons"];
+                    $action["Bonus"] = $out["Stats"]["Dexterity"] + $skill_bonus + $expertise_bonus;
                     $action["Dmg Bonus"] = $out["Stats"]["Dexterity"];
                 }else if (in_array("Ranged", $weapons_model->data[$equip]["Tags"])){
-                    $action["Bonus"] = $out["Stats"]["Dexterity"] + $out["Skills"]["Weapons"];
+                    $action["Bonus"] = $out["Stats"]["Dexterity"] + $skill_bonus + $expertise_bonus;
                     $action["Dmg Bonus"] = $out["Stats"]["Dexterity"];
                 }else if (in_array("Mechanical", $weapons_model->data[$equip]["Tags"])){
-                    $action["Bonus"] = $out["Stats"]["Wits"] + $out["Skills"]["Weapons"];
+                    $action["Bonus"] = $out["Stats"]["Wits"] + $skill_bonus + $expertise_bonus;
                     $action["Dmg Bonus"] = $out["Stats"]["Wits"];
                 }else{
-                    $action["Bonus"] = $out["Stats"]["Strength"] + $out["Skills"]["Weapons"];
+                    $action["Bonus"] = $out["Stats"]["Strength"] + $skill_bonus + $expertise_bonus;
                     $action["Dmg Bonus"] = $out["Stats"]["Strength"];
                 }
 
@@ -86,7 +105,7 @@ class CharacterSheetController extends Controller
                 $out["Actions"][$action['Title']] = $action;
                 $weapon_actions[$action['Title']] = $action;
                 if (in_array("Fine", $weapons_model->data[$equip]["Tags"])){
-                    $out["Manuevers"][$action['Title']] = $action;
+                    $out["Maneuvers"][$action['Title']] = $action;
                 }
 
                 if (in_array("Versatile D8", $weapons_model->data[$equip]["Tags"])){
@@ -141,34 +160,142 @@ class CharacterSheetController extends Controller
             if (isset($features_model->data[$feature])){
                 $curr_feature = $features_model->data[$feature];
                 $compendium[$curr_feature["Title"]] = $curr_feature["Description"];
-                if (isset($curr_feature['{{Weapon}}'])){
+                /**
+                 * Parse Weapon Actions
+                 */
+                if (isset($curr_feature['{{Weapon}}'])){ //done
                     foreach ($weapon_actions as $weapon=>$details) {
                         if (isset($curr_feature["{{Weapon}}"]["Exploit"])){
                             $details["Exploit"] = true;
                             $weapon = $weapon . "-Exploit";
                         }
-                        if (isset($curr_feature["Bonus Damage"])){
-                            $details["Additional Damage"][] = $curr_feature["Bonus Damage"];
+                        if (isset($curr_feature["{{Weapon}}"]["Bonus Damage"])){
+                            $details["Additional Damage"][] = $curr_feature["{{Weapon}}"]["Bonus Damage"];
                         }
                         $out["Actions"][$weapon] = $details;
                     }
+                    if (isset($curr_feature["{{Weapon}}"]["Description"])){
+                        $out["Features"][$curr_feature["Title"]] = $curr_feature["{{Weapon}}"]["Description"];
+                    }else{
+                        $out["Features"][$curr_feature["Title"]] = "";
+                    }
+                }elseif(isset($curr_feature["Action"])){ //unused
+
+                    
+                }elseif(isset($curr_feature["Maneuver"])){ //done
+                    dump($curr_feature);
+                    if(isset($curr_feature["Maneuver"]["Movement"])){
+                        $stride = 6;                     
+                        if(str_contains($curr_feature["Maneuver"]["Description"], "{{Stride}}")){
+                            $out["Maneuvers"][$curr_feature["Title"]] = str_replace("{{Stride}}", $stride, $curr_feature["Maneuver"]["Description"]);
+                        }else{
+                            $out["Maneuvers"][$curr_feature["Title"]] = $curr_feature["Maneuver"]["Description"];
+                        }
+                    }else{
+                        $title = $feature;
+                        if(isset($curr_feature["title"])){
+                            $title = $curr_feature["title"];
+                        }                      
+                        
+                        if(isset($curr_feature["Maneuver"]["Roll"])){
+                            $skill_bonus = 0;
+                            if(isset($curr_feature["Maneuver"]["Roll"][1]) && isset($out["Skills"][$curr_feature["Maneuver"]["Roll"][1]])){
+                                $skill_bonus = $out["Skills"][$curr_feature["Maneuver"]["Roll"][1]];
+                            }
+                            $expertise_bonus = 0;
+                            if(isset($curr_feature["Maneuver"]["Roll"][2]) && isset($out["Expertises"][$curr_feature["Maneuver"]["Roll"][2]])){
+                                $expertise_bonus = 2;
+                            }
+                            $out["Maneuvers"][$title]["Bonus"] = $out["Stats"][$curr_feature["Maneuver"]["Roll"][0]] + $skill_bonus + $expertise_bonus;                            
+                        }
+                        if(isset($curr_feature["Maneuver"]["Vs"])){
+                            $out["Maneuvers"][$title]["Vs"] = $curr_feature["Maneuver"]["Vs"];
+                        }
+                        $out["Maneuvers"][$title]["Description"] = $curr_feature["Maneuver"]["Description"];
+                    }
+                    if(isset($curr_feature["Maneuver"]["Description"])){
+                        $out["Features"][$curr_feature["Title"]] = $curr_feature["Maneuver"]["Description"];
+                    }else{
+                        $out["Features"][$curr_feature["Title"]] = "";
+                    }
+
+                }elseif(isset($curr_feature["Interaction"])){ //done
+                    $title = $feature;
+                    if (isset($curr_feature["Title"])){
+                        $title = $curr_feature["Title"];
+                    }
+                    $out["Interaction"][$title] = [];
+                    
+                    $skill_bonus = 0;
+                    if(isset($curr_feature["Interaction"]["Roll"][1]) && isset($out["Skills"][$curr_feature["Interaction"]["Roll"][1]])){
+                        $skill_bonus = $out["Skills"][$curr_feature["Interaction"]["Roll"][1]];
+                    }
+                    $expertise_bonus = 0;
+                    if(isset($curr_feature["Interaction"]["Roll"][2]) && isset($out["Expertises"][$curr_feature["Interaction"]["Roll"][2]])){
+                        $expertise_bonus = 2;
+                    }
+                    $out["Interaction"][$title]["Bonus"] = $out["Stats"][$curr_feature["Interaction"]["Roll"][0]] + $skill_bonus + $expertise_bonus;                            
+                                          
+                    
+                    if(isset($curr_feature["Interaction"]["Vs"])){
+                        $out["Interaction"][$title]["Vs"] = $curr_feature["Interaction"]["Vs"];
+                    }
+                    $out["Interaction"][$title]["Description"] = $curr_feature["Interaction"]["Description"];
+                    
+                    $out["Features"][$title] = "";
+                    if (isset($curr_feature["Interaction"]["Description"])){
+                        $out["Features"][$title] = $curr_feature["Interaction"]["Description"];
+                    }
+
+                }elseif(isset($curr_feature["Ritual"])){ //done
+                    $out["Rituals"][$feature] = $curr_feature["Ritual"]["Description"];
+                }elseif(isset($curr_feature["Stride"])){ //done
+                    $title = $feature;
+                    if(isset($curr_feature["Title"])){
+                        $title = $curr_feature["Title"];
+                    }
+
                     if (isset($curr_feature["Short Description"])){
-                        $out["Features"][$curr_feature["Title"]] = $curr_feature["Short Description"];
+
+                    }
+
+
+                    $out["Maneuvers"]["Stride"] = $curr_feature["Stride"];
+                }elseif(isset($curr_feature["Enigma"])){ //done
+                    $out["Spellcasting Feature"] = $curr_feature["Enigma"];
+                    $spellcasting_bonus = $out["Stats"][$curr_feature["Enigma"]["Spellcasting Bonus"][0]] + $out["Skills"][$curr_feature["Enigma"]["Spellcasting Bonus"][1]];
+                    $out["Features"][$curr_feature["Title"]] = "Spellcasting Bonus: +" . strval($spellcasting_bonus);
+                    if(isset($curr_feature["Enigma"]["Base Burn"])){
+                        $out["Features"][$curr_feature["Title"]] .= " Base Burn: ". $curr_feature["Enigma"]["Base Burn"];
+                        $out["Burn"] = array("Base Burn"=>$curr_feature["Enigma"]["Base Burn"], "Current Burn"=>$curr_feature["Enigma"]["Base Burn"]);
+                    }
+
+                    if(isset($curr_feature["Enigma"]["Fervor"])){
+                        $out["Fervor"] = 0;
+                    }
+
+
+                    $out["Features"][$curr_feature["Title"]] .= "<br/>";
+                    if(isset($curr_feature["Enigma"]["Cantrips"])){
+                        $out["Features"][$curr_feature["Title"]] .= " Cantrips: " . $curr_feature["Enigma"]["Cantrips"];
+                    }
+                    $out["Features"][$curr_feature["Title"]] .= " Prepare: " . strval($out["Stats"][$curr_feature["Enigma"]["Prepare"][0]] + $out["Skills"][$curr_feature["Enigma"]["Prepare"][1]]);
+                    $out["Features"][$curr_feature["Title"]] .= "/" . $curr_feature["Enigma"]["To Prepare"];
+
+                    $out["Features"][$curr_feature["Title"]] .= " of up to Tier: " . $curr_feature["Enigma"]["Max Tier"];
+                    
+                }else{
+                    if(isset($curr_feature["Description"])){
+                        $out["Features"][$curr_feature["Title"]] = $curr_feature["Description"];
                     }else{
                         $out["Features"][$curr_feature["Title"]] = "";
                     }
                     
-                }elseif(isset($curr_feature["Maneuver"])){
-                    dump($curr_feature["Maneuver"]);
-
-                }elseif(isset($curr_feature["Interaction"])){
-                    dump($curr_feature["Interaction"]);
-                }elseif(isset($curr_feature["Ritual"])){
-
                 }
             }
         }
 
+        
         dd($character, $out, $compendium);
     }
 }
